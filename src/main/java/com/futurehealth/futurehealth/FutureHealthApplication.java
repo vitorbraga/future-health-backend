85f8d28251606e768bbcc268a9d57c2b6d45c17f
Vitor Braga <vitor.braga7@gmail.com>
2018-09-05 07:22:53 -0300

    First commit

---
+++ src/main/java/com/futurehealth/futurehealth/FutureHealthApplication.java
@@ -0,0 +1,12 @@
+package com.futurehealth.futurehealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureHealthApplication.class, args);
+	}
+}
